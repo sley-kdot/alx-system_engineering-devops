7091e4d397e2ba132eb952bb9a7b5df31bf8f663
Kingsley Okonmah <60091919+sley-kdot@users.noreply.github.com>
2024-01-23 12:23:29 +0100

    remove the ensure property

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 # client-side configuration with puppet
 
 file_line {'make use of private key':
-  ensure => present,
   path   => '/etc/ssh/ssh-config',
   line   => 'IdentityFile ~/.ssh/school',
 }
 
 file_line {'refuse to authenticate using a passwd':
-  ensure => present,
   path   => '/etc/ssh/ssh-config',
   line   => 'PasswordAuthentication',
 }
