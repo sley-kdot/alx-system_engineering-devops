38dc902f58ad4062317388bd1bd125d9087475e6
Kingsley Okonmah <60091919+sley-kdot@users.noreply.github.com>
2024-01-23 12:27:08 +0100

    fix: typo error for the path property

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # client-side configuration with puppet
 
 file_line {'make use of private key':
-  path   => '/etc/ssh/ssh-config',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
 
 file_line {'refuse to authenticate using a passwd':
-  path   => '/etc/ssh/ssh-config',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication',
 }
