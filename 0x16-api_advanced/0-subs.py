897ca37b3db534937caa40e3251de79a44496d81
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-08-17 23:14:32 +0100

    feat: updated the script to handle edge cases

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,11 @@ from sys import argv
 
 def number_of_subscribers(subreddit):
     subreddit = argv[1]
-    url = f"https://www.reddit.com/r/subreddit/about.json"
-    res = requests.get(url).json()
-    data = res["data"].get("subscribers")
-    return data
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    res = requests.get(url)
+    if res.status_code == 200:
+        data = res.json()
+        result = data["data"].get("subscribers")
+        return result
+    else:
+        return 0
