49638b2014bdb5d035370681fab262b8dfae95ed
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-08-18 18:24:38 +0100

    fix: uncomment function name

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,18 +6,18 @@ import requests
 from sys import argv
 
 
-# def top_ten(subreddit):
-subreddit = argv[1]
-url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-params = {"limit": 10}
-res = requests.get(url, params=params)
-if res.status_code == 200:
-    data = res.json()
-    var = []
-    for k, v in data.items():
-        if k == "data":
-            var = v["children"]
-            for i in range(len(var)):
-                print(var[i]["data"].get("title"))
-else:
-    print(None)
+def top_ten(subreddit):
+    subreddit = argv[1]
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"limit": 10}
+    res = requests.get(url, params=params)
+    if res.status_code == 200:
+        data = res.json()
+        var = []
+        for k, v in data.items():
+            if k == "data":
+                var = v["children"]
+                for i in range(len(var)):
+                    print(var[i]["data"].get("title"))
+    else:
+        print(None)
