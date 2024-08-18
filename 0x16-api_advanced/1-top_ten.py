69f641000f347b3004743a18b7942011b05ab6eb
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-08-18 18:21:40 +0100

    feat: function that queries an API endpoint

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a function that queries an API endpoint
+"""
+import requests
+from sys import argv
+
+
+# def top_ten(subreddit):
+subreddit = argv[1]
+url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+params = {"limit": 10}
+res = requests.get(url, params=params)
+if res.status_code == 200:
+    data = res.json()
+    var = []
+    for k, v in data.items():
+        if k == "data":
+            var = v["children"]
+            for i in range(len(var)):
+                print(var[i]["data"].get("title"))
+else:
+    print(None)
